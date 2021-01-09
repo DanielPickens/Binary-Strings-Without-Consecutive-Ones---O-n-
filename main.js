a282ac2b003c9463b7f6a6194368fce5adbfdda4
Dan Pickens <72703981+DanielPickens@users.noreply.github.com>
2021-01-09 16:55:32 -0700

    first
    added my code

---
+++ main.js
@@ -0,0 +1,12 @@
+class Solution {
+
+    countBinaryStrings(n) {
+         let dp = new Array(n + 1);  
+        dp[0] = 1
+        dp[1] = 2
+        for (let i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2]; 
+        }
+        return dp[n]; 
+   }
+}  
